8ff70dcd8733c9c159df50a02ebf421b3d3d653c
chae2 <saintcharry22@gmail.com>
2026-05-19 15:27:58 +0900

    feat: pdu 생성 로직

--- coordinator/L3_msg.h
+++ coordinator/L3_msg.h
@@ -1,1 +1,25 @@
 #include "mbed.h"
+
+// Coordinator L3 message type
+#define L3_MSG_TYPE_MCH 0
+#define L3_MSG_TYPE_REC 1
+#define L3_MSG_TYPE_WAIT_PAIR 2
+// Trader L3 message type
+#define L3_MSG_TYPE_TXN 3
+#define L3_MSG_TYPE_CNF 4
+
+// L3 message format
+#define L3_MSG_OFFSET_TYPE 0  // 0번째 칸: 패킷 타입 (MCH, TXN 등)
+#define L3_MSG_OFFSET_SEQ 1   // 1번째 칸: 시퀀스 넘버 (SEQ NUM)
+#define L3_MSG_OFFSET_SRCID 2
+#define L3_MSG_OFFSET_DESTID 3
+#define L3_MSG_OFFSET_DATA 4  // 4번째 칸: 실제 데이터 내용(Payload)
+
+#define L3_MSG_MAXDATASIZE 1024
+#define L3_MSG_MAX_SEQNUM 1024
+
+int L3_msg_checkMsgType(uint8_t* msg);
+uint8_t L3_msg_encodeMsg(uint8_t* msg, uint8_t type, uint8_t seq, uint8_t srcId,
+                         uint8_t destId, uint8_t* data, int len);
+uint8_t L3_msg_getSeq(uint8_t* msg);
+uint8_t* L3_msg_getData(uint8_t* msg);
