6a72a8eb7eaae1da4d09c06d045384349b62568b
chae2 <saintcharry22@gmail.com>
2026-06-07 19:52:32 +0900

    feat: timer, protocol_parameters.h sync

--- trader/L3_timer.cpp
+++ trader/L3_timer.cpp
@@ -15,24 +15,25 @@
 #include "protocol_parameters.h"
 
 static Timeout timer;
-static uint8_t timerStatus = 0;    // 타이머 동작 중 여부 (1: 동작, 0: 정지)
-
+static uint8_t timerStatus = 0;  // 타이머 동작 중 여부 (1: 동작, 0: 정지)
 
 // 타임아웃 발생 시 자동 호출 (인터럽트 핸들러)
 void L3_timer_timeoutHandler(void) {
   timerStatus = 0;
-  L3_event_setEventFlag(L3_event_timeout);   // FSM에 timeout 이벤트 알림
+  L3_event_setEventFlag(L3_event_timeout);  // FSM에 timeout 이벤트 알림
 }
 
 // 타이머 시작
 void L3_timer_startTimer(uint8_t waitSec) {
-  timer.attach(L3_timer_timeoutHandler, waitSec);
-  timerStatus = 1;            // waitSec초 후에 timeoutHandler를 자동 호출하도록 등록
+  uint8_t waitTime =
+      L3_MINWAITTIME + rand() % (L3_MAXWAITTIME - L3_MINWAITTIME);
+  timer.attach(L3_timer_timeoutHandler, waitTime);
+  timerStatus = 1;  // waitTime초 후에 timeoutHandler를 자동 호출하도록 등록
 }
 
-// 타이머 중단 
+// 타이머 중단
 void L3_timer_stopTimer() {
-  timer.detach();     // 등록된 핸들러 해제 → 타임아웃 이벤트 발생 안 함
+  timer.detach();  // 등록된 핸들러 해제 → 타임아웃 이벤트 발생 안 함
   timerStatus = 0;
 }
 
--- trader/protocol_parameters.h
+++ trader/protocol_parameters.h
@@ -1,14 +1,20 @@
 #define DBGMSG_L2 0  // L2 디버그 출력 제어 (1: 활성화)
 #define DBGMSG_L3 0  // L3 디버그 출력 제어 (1: 활성화)
 
-#define L3_MAXDATASIZE 1024    // L3 데이터 크기 (바이트)
+#define L3_MAXDATASIZE 1024  // L3 데이터 크기 (바이트)
+#define L3_MAXWAITTIME 30
+#define L3_MINWAITTIME 10
 
-#define L2_ARQ_MAXRETRANSMISSION 10   // ARQ 최대 재전송 횟수
-#define L2_ARQ_MAXWAITTIME 5    // ARQ 최대 대기 시간(초)
-#define L2_ARQ_MINWAITTIME 2    // ARQ 최소 대기 시간(초)
-#define L1_FREQCHANNEL 1        // 주파수 채널(x 1MHz)
+#define L2_ARQ_MAXRETRANSMISSION 10  // ARQ 최대 재전송 횟수
+#define L2_ARQ_MAXWAITTIME 5         // ARQ 최대 대기 시간(초)
+#define L2_ARQ_MINWAITTIME 2         // ARQ 최소 대기 시간(초)
+#define L1_FREQCHANNEL 1             // 주파수 채널(x 1MHz)
 
-#define L3_PAIR_TIMEOUT    5   // 페어링 응답 대기 타임아웃(초)
-#define L3_MCH_TIMEOUT     5   // 매칭 결과 대기 타임아웃(초)
+#define L3_PAIR_TIMEOUT 5  // 페어링 응답 대기 타임아웃(초)
+#define L3_MCH_TIMEOUT 5   // 매칭 결과 대기 타임아웃(초)
 
-#define L3_COORDINATOR_ID  0   // 코디네이터 노드 ID (고정값)
+#define L3_COORDINATOR_ID 0  // 코디네이터 노드 ID (고정값)
+#define L3_MIN_RSSI -60      // 임의 설정
+
+// 중간 위치 임의 설정(숙명여대 눈꽃광장홀)
+#define AVG_LOC 4312  // 04312, 우편번호는 636***까지 표현 가능
