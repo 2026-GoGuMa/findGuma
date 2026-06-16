c2efe31dab4b568c6e8eaa31170ad0559e8d9790
chae2 <saintcharry22@gmail.com>
2026-06-16 17:41:51 +0900

    refactor: main 수정
    - 주석 깨진 거
    - printf문에 조건 추가

--- trader/main.cpp
+++ trader/main.cpp
@@ -1,62 +1,61 @@
 ﻿#include "L2_FSMmain.h"
-#include "L3_FSMmain.h"
 #include "L3_FSMevent.h"
+#include "L3_FSMmain.h"
 #include "mbed.h"
 #include "protocol_parameters.h"
 #include "string.h"
 
-// ?쒕━???ы듃 (PC? ?듭떊??
+// serial port interface
 Serial pc(USBTX, USBRX);
 
-// ?꾩뿭 蹂??(?섏젙 湲덉?) ------------------------------------------
-uint8_t input_thisId  = 1;  // ???몃뱶??ID
+uint8_t input_thisId = 1;  // 초기화
 
-// ?꾨줈洹몃옩 ?쒖옉??------------------------------------------------
 // 시리얼 인터럽트 콜백: 사용자가 1 또는 0 입력 시 이벤트 세팅
 void onSerialRx(void) {
   char c = pc.getc();
-  if (c == '1') L3_event_setEventFlag(L3_event_userAccept);
-  else if (c == '0') L3_event_setEventFlag(L3_event_userReject);
+  if (c == '1')
+    L3_event_setEventFlag(L3_event_userAccept);
+  else if (c == '0')
+    L3_event_setEventFlag(L3_event_userReject);
 }
 
 int main(void) {
   pc.printf(
       "------------------ protocol stack starts! --------------------------\n");
 
-  // ?ъ슜???낅젰 諛쏄린
+  // 아이디 세팅
   pc.printf(":: ID for this node : ");
   pc.scanf("%d", &input_thisId);
 
-  uint8_t  input_isSeller = 0;  // ?먮ℓ???щ? (0: 援щℓ?? 1: ?먮ℓ??
-  uint8_t  input_goods    = 0;  // ?곹뭹 醫낅쪟
-  uint16_t input_price    = 0;  // ?щ쭩 媛寃?
+  uint8_t input_isSeller = 0;
+  uint8_t input_goods = 0;
+  uint16_t input_price = 0;
 
   pc.printf(":: isSeller (0=buyer / 1=seller) : ");
   pc.scanf("%d", &input_isSeller);
 
-  pc.printf(":: goods type : ");
+  pc.printf(":: goods type 256 이하 : ");
   pc.scanf("%d", &input_goods);
 
-  pc.printf(":: price ($) : ");
+  pc.printf(":: price ($) 65536 이하 : ");
   pc.scanf("%d", &input_price);
 
   pc.getc();
 
   // ?낅젰媛??뺤씤 異쒕젰
   pc.printf("Trader id=%u  coord=%u  isSeller=%u  goods=%u  price=$%u\n",
-             input_thisId, L3_COORDINATOR_ID, input_isSeller,
-             input_goods, input_price);
+            input_thisId, L3_COORDINATOR_ID, input_isSeller, input_goods,
+            input_price);
 
-  // FSM 珥덇린??
+  // FSM 초기화
   L2_initFSM(input_thisId);
-  L3_initFSM(input_thisId, L3_COORDINATOR_ID, input_isSeller,
-              input_goods, input_price);
+  L3_initFSM(input_thisId, L3_COORDINATOR_ID, input_isSeller, input_goods,
+             input_price);
 
   pc.attach(&onSerialRx, Serial::RxIrq);  // 시리얼 인터럽트 등록
 
-  // 硫붿씤 猷⑦봽 : L2 ??L3 ?쒖꽌濡?諛섎났 ?ㅽ뻾
   while (1) {
-    L2_FSMrun();   // L2 FSM (臾댁꽑 ?≪닔??泥섎━)
-    L3_FSMrun();   // L3 FSM (嫄곕옒 ?묒긽 泥섎━)
+    L2_FSMrun();  // L2 FSM (臾댁꽑 ?≪닔??泥섎━)
+    L3_FSMrun();  // L3 FSM (嫄곕옒 ?묒긽 泥섎━)
   }
 }
